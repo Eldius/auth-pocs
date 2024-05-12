eb7fd4693055e1d9c2b6b9b4cc621e84ec2dd151
eldius <eldiosantos@gmail.com>
2024-05-11 23:14:42 -0300

    Some more code...

--- basic-auth/internal/api/api_test.go
+++ basic-auth/internal/api/api_test.go
@@ -14,10 +14,12 @@ import (
 func TestHome(t *testing.T) {
 
 	assert.Nil(t, logging.SetupLogs("basic-auth-test", true))
+	db := persistence.InitDB()
+	defer func() {
+		_ = db.Close()
+	}()
 
 	t.Run("given an unauthenticated request should return 401", func(t *testing.T) {
-		db := persistence.InitDB()
-
 		mux := http.NewServeMux()
 		mux.HandleFunc("/", Home)
 		server := httptest.NewServer(hmiddleware.LoadMiddlewares(mux, middleware.WithBasicAuthHandler(db)))
@@ -29,21 +31,13 @@ func TestHome(t *testing.T) {
 	})
 
 	//t.Run("given an authenticated request should return 200", func(t *testing.T) {
-	//	db := persistence.InitDB()
-	//	defer func() {
-	//		_ = db.Close()
-	//	}()
-	//
 	//	mux := http.NewServeMux()
 	//	mux.HandleFunc("/", Home)
 	//	server := httptest.NewServer(hmiddleware.LoadMiddlewares(mux, hmiddleware.WithLoggingHandler(), middleware.WithBasicAuthHandler(db)))
 	//	defer server.Close()
 	//
 	//	req, err := http.NewRequest(http.MethodGet, server.URL+"/", nil)
-	//	buff := bytes.NewBuffer(nil)
-	//	w := base64.NewEncoder(base64.StdEncoding, buff)
-	//	_, _ = w.Write([]byte("root:12345"))
-	//	req.Header.Add("Authorization", fmt.Sprintf("Basic %s", buff.String()))
+	//	req.Header.Add("Authorization", fmt.Sprintf("Basic %s", base64.StdEncoding.EncodeToString([]byte("root:12345"))))
 	//	resp, err := http.Get(server.URL + "/")
 	//	assert.NoError(t, err)
 	//	assert.Equal(t, http.StatusOK, resp.StatusCode)
